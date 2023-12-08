b40336e78350ffb79ed209b95d08617cda1a4555
Nate Ferreira <nferreira1729@gmail.com>
2023-12-08 16:31:07 -0500

    fix: update code to use newer author model

--- src/author/mod.rs
+++ src/author/mod.rs
@@ -7,8 +7,8 @@ pub mod queries;
 
 pub fn author_config(cfg: &mut web::ServiceConfig) {
     cfg.service(
-        web::scope("/author")
-            .service(web::resource("list").route(web::get().to(handlers::get_authors)))
+        web::scope("/authors")
+            .service(web::resource("").route(web::get().to(handlers::get_authors)))
             .service(web::resource("create").route(web::post().to(handlers::create_author))),
     );
 }
