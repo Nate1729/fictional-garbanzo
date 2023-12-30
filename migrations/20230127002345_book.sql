60cfabe24989286447d9c125cd7b20763378e3cd
Nate Ferreira <nferreira1729@gmail.com>
2023-12-29 20:03:04 -0500

    refactor: change migrations to work with postgresql

--- migrations/20230127002345_book.sql
+++ migrations/20230127002345_book.sql
@@ -1,15 +1,18 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS author (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  first_name TEXT NOT NULL,
-  last_name TEXT NOT NULL
+  id  bigint,
+  first_name VARCHAR(128) NOT NULL,
+  last_name VARCHAR(128) NOT NULL,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS book (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT NOT NULL,
-  year_published TEXT NOT NULL,
-  author_id INTEGER NOT NULL,
-  rented_to TEXT,
-  FOREIGN KEY(author_id) REFERENCES author(id)
+  id bigint,
+  title VARCHAR(256) NOT NULL,
+  year_published int NOT NULL,
+  author_id bigint NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_author
+    FOREIGN KEY(author_id)
+      REFERENCES author(id)
 );
